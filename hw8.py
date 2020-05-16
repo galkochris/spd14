97ba42cc010525786f9f61b40015d4889b1f7c16
galkochris <galkochris@gmail.com>
2020-05-15 22:08:44 -0700

    hw8

---
+++ hw8.py
@@ -0,0 +1,6 @@
+#Two Original Problems
+
+#1: Given a string, determine if it represents a number and convert it into base 10
+
+#2: Given a list of touple pairs of words and numbers, devise an algorithm that sorts the pairs in the most in order order, accounting
+#both numerical order and alphabetical order
